273b4d3b014f7ca5919f97dfceb5ec8e4d82bf7b
William Knowles <wknowles@users.noreply.github.com>
2016-10-01 21:43:09 -0400

    add alt js and simplified data

--- js/rental.js
+++ js/rental.js
@@ -1,19 +1,18 @@
-d3.json("data/active_housing.json", function(data) {
-var dateExtent = d3.extent(data, function(d) { return d.issue_date; });
-console.log(data.length);
-
-var licensesTotalByYear = d3.nest()
-  .key(function(d) { return d.issue_date.split("-")[0]; }).sortKeys(d3.ascending)
-  .rollup(function(d) { return d.length; })
-  .entries(data);
-console.log(JSON.stringify(licensesTotalByYear));
-
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 20, bottom: 50, left: 70},
     width = 960 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
 
-// append the svg obgect to the body of the page
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// define the line
+var valueline = d3.line()
+    .x(function(d) { return x(d.key); })
+    .y(function(d) { return y(d.value); });
+
+// append the svg object to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
 var svg = d3.select("body").append("svg")
@@ -23,59 +22,59 @@ var svg = d3.select("body").append("svg")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-svg.selectAll("rect")
-   .data(licensesTotalByYear)
-   .enter()
-   .append("rect")
-   .attr("x", function(d, i) {
-    return i * (width / licensesTotalByYear.length);
-    })
-   .attr("y", function(d) {
-    return height - (d.value / 5)// height - data value
-    })
-   .attr("width", width / licensesTotalByYear.length)
-   .attr("height", function(d) {
-    return (d.value / 5);
-    })
-   .attr("fill", "teal");
+// parse the date / time
+var parseTime = d3.timeParse("%Y-%m-%d");
 
-svg.selectAll("text")
-  .data(licensesTotalByYear)
-  .enter()
-  .append("text")
-  .text(function(d) {
-      return d.value;
-  })
-  .attr("x", function(d, i) {
-  return i * (width / licensesTotalByYear.length) + (width / licensesTotalByYear.length) / 2;
-  })
-  .attr("y", function(d) {
-      return height - (d.value / 5) - 10;
-  })
-  .attr("font-family", "sans-serif")
-  .attr("text-anchor", "middle")
-  .attr("font-size", "11px")
-  .attr("fill", "black");
+// import data
+d3.csv("data/active_rental.csv", function(error, data) {
+  if (error) throw error;
 
-// set the ranges
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+  // format the data
+  data.forEach(function(d) {
+      d.issue_date = parseTime(d.issue_date);
+  });
 
-  // Scale the range of the data
-  x.domain(d3.extent(licensesTotalByYear, function(d) { return d.key; }));
-  y.domain([0, d3.max(licensesTotalByYear, function(d) { return d.value; })]);
+// keys are always treated as strings -- https://stackoverflow.com/questions/25576853/d3-nest-formatting-date-incorrectly
+
+// nest, key and rollups
+var licensesTotalByYear = d3.nest()
+  .key(function(d) { return d.issue_date.getFullYear(); }).sortKeys(d3.ascending)
+  .rollup(function(d) { return d.length; })
+  .entries(data);
+console.log(JSON.stringify(licensesTotalByYear));
+
+// Scale the range of the data
+x.domain(d3.extent(licensesTotalByYear, function(d) { return d.key; }));
+y.domain([0, d3.max(licensesTotalByYear, function(d) { return d.value; })]);
+
+// Add the valueline path.
+  svg.append("path")
+      .data([licensesTotalByYear])
+      .attr("class", "line")
+      .attr("d", valueline);
 
 // Add the x Axis
 svg.append("g")
     .attr("transform", "translate(0," + (height)  + ")")
     .call(d3.axisBottom(x));
-// Add the y Axis
-svg.append("g")
-    .call(d3.axisLeft(y));
+// text label for the x axis
 svg.append("text")
       .attr("transform",
             "translate(" + (width/2) + " ," +
                            (height + margin.top + 20) + ")")
       .style("text-anchor", "middle")
+      .attr("font-family", "sans-serif")
       .text("Year");
+// Add the y Axis
+svg.append("g")
+    .call(d3.axisLeft(y));
+// text label for the y axis
+  svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .attr("font-family", "sans-serif")
+      .text("Value");
 });
