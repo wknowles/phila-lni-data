b48bba5a94ceccf61e1428ce8c9d9be643bdf8cc
William Knowles <wknowles@users.noreply.github.com>
2016-10-03 09:27:17 -0400

    add tooltip to api.js

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 20, right: 20, bottom: 50, left: 70},
-    width = 700 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+    width = 900 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
 
 // set the ranges
 var x = d3.scaleTime().range([0, width]);
@@ -23,36 +23,38 @@ var svg = d3.select("body").append("svg")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // parse the date / time
-//var parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S.%L");
+//var parseTime = d3.timeParse("%Y-%m-%d");
 
 /////////
 
 // import data from api
 d3.csv("https://data.phila.gov/resource/st4m-d4h3.csv?"
   + "$select=issuedate,expirationdate,censustract,fulladdress"
-  + "&$where=issuedate>%272016-08-01T00:00:00.000%27"
+  + "&$where=issuedate>%272016-07-01T00:00:00.000%27"
   + "&licensestatus=Active"
   + "&revenuecode=3202"
   + "&$order=issuedate"
-//  + "&$limit=100"
+  // + "&$limit=10"
+  // app_token should be REDACTED but whatevs... security :-(
   + "&$$app_token=8nwKL3zJBeBIdPKTZzwuKspfh", function (error, data) {
   if (error) throw error;
 
   // Make sure our numbers are really numbers
   data.forEach(function (d) {
     d.censustract = +d.censustract;
-    d.issuedate = moment(d.issuedate);
+    d.issuedate = moment(d.issuedate).format("DD");
+    //d.issuedate = parseTime(d.issuedate);
     d.expirationdate = moment(d.expirationdate);
   });
 
   var licenseByDate = d3.nest()
-  .key(function(d) { return d.issuedate; }).sortKeys(d3.ascending)
+  .key(function(d) { return d.issuedate }).sortKeys(d3.ascending)
+  //.key(function(d) { return d.censustract; })
   .rollup(function(d) { return d.length; })
   .entries(data);
-  console.log(JSON.stringify(licenseByDate));
 
   // Scale the range of the data
-  x.domain(d3.extent(licenseByDate, function(d) { return d.key; }));
+  x.domain([d3.min(licenseByDate, function(d) { return d.key; }), d3.max(licenseByDate, function(d) { return d.key; })]);
   y.domain([0, d3.max(licenseByDate, function(d) { return d.value; })]);
 
   // Add the graphline path.
@@ -89,6 +91,41 @@ d3.csv("https://data.phila.gov/resource/st4m-d4h3.csv?"
       .style("text-anchor", "middle")
       .text("Total");
 
-  //console.log(data)
-  //console.log(JSON.stringify(data));
+
+  var bisectDate = d3.bisector(function(d) { return d.key; }).left;
+
+  var focus = svg.append("g")
+      .attr("class", "focus")
+      .style("display", "none");
+
+  focus.append("circle")
+      .attr("r", 4.5);
+
+  focus.append("text")
+      .attr("x", 0)
+      .attr("dy", -10);
+
+  svg.append("rect")
+      .attr("class", "overlay")
+      .attr("width", width)
+      .attr("height", height)
+      .on("mouseover", function() { focus.style("display", null); })
+      .on("mouseout", function() { focus.style("display", "none"); })
+      .on("mousemove", mousemove);
+
+  function mousemove() {
+    var x0 = x.invert(d3.mouse(this)[0]),
+        i = bisectDate(licenseByDate, x0, 1),
+        d0 = licenseByDate[i - 1],
+        d1 = licenseByDate[i],
+        d = x0 - d0.key > d1.key - x0 ? d1 : d0;
+    focus.attr("transform", "translate(" + x(d.key) + "," + y(d.value) + ")");
+    focus.select("text").text(d.value);
+  }
+
+  console.log(JSON.stringify(licenseByDate));
+  //console.log(licenseByDate)
+  console.log(data)
+
 });
+
