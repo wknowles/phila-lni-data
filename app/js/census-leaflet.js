9f7db773a2f7a465b20ef302866c684bd598f8eb
William Knowles <wknowles@users.noreply.github.com>
2016-10-14 13:56:49 -0400

    d3 choropleth arrangement

--- app/js/census-leaflet.js
+++ app/js/census-leaflet.js
@@ -1,5 +1,6 @@
 var map = L.map('leaflet').setView([40.008, -75.1652], 11);
     map.options.minZoom = 11;
+    map.options.maxZoom = 14;
 
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>' }).addTo(map);
 
--- app/js/d3-choropleth.js
+++ app/js/d3-choropleth.js
@@ -1,8 +1,8 @@
 // set up map
 var width = 470,
-  height = 500;
+  height = 580;
 
-var div = d3.select('#chart').append('div')
+var tooltip = d3.select('body').append('div')
     .attr('class', 'tooltip');
 
 var svg = d3.select('#chart').append('svg')
@@ -17,10 +17,10 @@ var projection = d3.geoMercator()
 var path = d3.geoPath()
     .projection(projection);
 
-var censusTract = d3.select('#censusTract').insert('p')
-    .attr('class', 'title');
-var censusTotal = d3.select('#censusTotal').insert('p')
-    .attr('class', 'title');
+// var censusTract = d3.select('#censusTract').insert('p')
+//     .attr('class', 'title');
+// var censusTotal = d3.select('#censusTotal').insert('p')
+//     .attr('class', 'title');
 
 // read map file and data
 queue()
@@ -70,12 +70,9 @@ function ready(error, map, data) {
 
 //Adding mouseevents
   .on('mouseover', function(d) {
-    d3.select(this).transition().duration(300).style('opacity', 1);
-    div.transition().duration(300)
-    .style('opacity', 1);
-    censusTract.html(d3.format('.0f')(d.properties.NAME10));
-    censusTotal.html(tractById[d3.format('.0f')(d.properties.NAME10)]);
-    //div.html('Census tract = ' + d3.format('.0f')(d.properties.NAME10) + '<br> No. of licenses = ' + tractById[d3.format('.0f')(d.properties.NAME10)]);
+    tooltip.html('Census tract = ' + d3.format('.0f')(d.properties.NAME10) + '<br> No. of licenses = ' + tractById[d3.format('.0f')(d.properties.NAME10)])
+        .style('left', (d3.event.pageX - 70) + 'px')
+        .style('top', (d3.event.pageY + 30) + 'px');
     // console.log(d.properties.NAME10);
     // console.log(d3.format(".0f")(d.properties.NAME10));
   })
@@ -83,13 +80,19 @@ function ready(error, map, data) {
     d3.select(this)
     .transition().duration(300)
     .style('opacity', 1);
-    div.transition().duration(300)
+    tooltip.transition().duration(300)
     .style('opacity', 1);
-    censusTract.html('-');
-    censusTotal.html('-');
+    // tooltip.html('-');
+    // tooltip.html('-');
   });
 
-
+  svg.append('text')
+        .attr('x', (width / 2))
+        .attr('y', 520)
+        .attr('text-anchor', 'middle')
+        .style('font-size', '16px')
+        .style('text-decoration', 'underline')
+        .text('Active rental licenses by census tract');
 
 //console.log(d3.extent(data, function(d) { return +d.count_censustract; }))
 //console.log(d3.selectAll(data, function(d) { return +d.count_censustract; }).size())
@@ -98,41 +101,50 @@ function ready(error, map, data) {
 //console.log(JSON.stringify(totalById))
 
 //Add key to Choropleth
-  var w = 250, h = 60;
+  var w = 60, h = 180;
 
   var key = svg.append('g')
+            .attr('id', 'key')
+            .attr('class', 'key')
             .attr('width', w)
             .attr('height', h)
-            .attr('id', 'key')
-            .attr('class', 'key');
+            .attr('transform','translate(360, 280)');
 
   var legend = key.append('defs')
                 .append('svg:linearGradient')
                 .attr('id', 'gradient')
-                .attr('x1', '0%')
+                .attr('x1', '100%')
                 .attr('y1', '0%')
                 .attr('x2', '100%')
-                .attr('y2', '0%')
+                .attr('y2', '100%')
                 .attr('spreadMethod', 'pad');
 
-  legend.append('stop').attr('offset', '0%').attr('stop-color', minimumColor).attr('stop-opacity', 1);
-  legend.append('stop').attr('offset', '100%').attr('stop-color', maximumColor).attr('stop-opacity', 1);
+  legend.append('stop').attr('offset', '0%').attr('stop-color', maximumColor).attr('stop-opacity', 1);
+  legend.append('stop').attr('offset', '100%').attr('stop-color', minimumColor).attr('stop-opacity', 1);
 
-  key.append('rect').attr('width', w - 10).attr('height', h - 40).style('fill', 'url(#gradient)').attr('transform', 'translate(5,0)');
+  key.append('rect').attr('width', w - 40).attr('height', h - 20).style('fill', 'url(#gradient)').attr('transform', 'translate(0,10)');
 
-  var x = d3.scaleLinear()
-          .range([ 0, w - 10])
+  var y = d3.scaleLinear()
+          .range([ h - 20, 0])
           .domain(d3.extent(data, function(d) { return +d.count_censustract; }));
 
-  var xAxis = d3.axisBottom(x);
+  var yAxis = d3.axisRight(y);
   key.append('g')
-  .attr('class', 'z axis')
-  .attr('transform', 'translate(5,20)')
-  .call(xAxis).selectAll('text')
+  .attr('class', 'y axis')
+  .attr('transform', 'translate(24,10)')
+  .call(yAxis).selectAll('text')
             .style('text-anchor', 'end')
-            .attr('dx', '-.8em')
-            .attr('dy', '.15em')
-            .attr('transform', 'rotate(-65)' );
+            .attr('dx', '2.5em')
+            .attr('dy', '0em');
+            //.attr('transform', 'rotate(-65)' );
+
+  // .attr('class', 'y axis')
+  // .attr('transform', 'translate(24,10)')
+  // .call(yAxis).append('text')
+  //     .attr('transform', 'rotate(-90)')
+  //     .attr('y', 30).attr('dy', '1em')
+  //     .style('text-anchor', 'end')
+  //     .text('Number of');
 //end of key
 
 }// <-- End of Choropleth drawing
